3c73a9afd05ae773f44aeedbb81b1dc31606cfc9
vedantsaxena <tanmay.saxena@gmail.com>
2020-05-20 11:20:46 +0530

    first commit

---
+++ Sling.js
@@ -0,0 +1,29 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.009,
+            length: 10
+        }
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            stroke(48,22,8);
+            strokeWeight(2);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+    
+}
